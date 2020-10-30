266afad8128afba0539bf0c5bab43b0402308d64
magistrfox <magistrfox.business@gmail.com>
2020-10-31 00:01:22 +0300

    fix errors

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import styles from "../styles/Todos.module.css";
 import SingleTodo from "./SingleTodo";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 
 const Todos = () => {
   const todos = useSelector(state => state.todos);
-  const dispatch = useDispatch();
 
   return (
       <Droppable droppableId={"todos-droppable"}>
@@ -19,9 +18,6 @@ const Todos = () => {
             {todos.map((item, i) => (
               (
                   <SingleTodo
-                    innerRef={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
                     index={i}
                     key={item.id}
                     id={item.id}
